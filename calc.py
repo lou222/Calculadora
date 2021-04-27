6bdf5a4efbb9a7e12e99a9bd00169ba2dd7e6854
Luiz b. Neto <luizbneto45@hotmail.com>
2021-04-27 19:21:58 -0300

    Calculadora simples com funcao de somar

---
+++ calc.py
@@ -0,0 +1,10 @@
+#Calculadora v0.1 (Calculadora de somar simples)
+
+def function_Plus(num1, num2):
+    return num1 + num2
+
+num1 = int(input('Insira primeiro numero: '))
+num2 = int(input('Insira segundo numero: '))
+
+resut = function_Plus(num1, num2)
+print(resut)
