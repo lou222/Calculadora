741920ac70743320b65ac0f744519cc0a2460d9b
Luiz Batista Neto <luizbneto45@hotmail.com>
2022-08-15 20:26:39 -0300

    Update calc.py
    Mais operações

--- calc.py
+++ calc.py
@@ -1,10 +1,35 @@
-#Calculadora v0.1 (Calculadora de somar simples)
+#Calculadora v0.1 (Calculadora simples)
+
+num1 = int(input('Insira primeiro numero: '))
+
+num2 = int(input('Insira segundo numero: '))
+
+decisao = int(input('Qual operação você deseja realizar?\n' + 
+'1 - Soma, 2 - Subtrair, 3 - Multi, 4 - Div\n'))
 
 def function_Plus(num1, num2):
     return num1 + num2
 
-num1 = int(input('Insira primeiro numero: '))
-num2 = int(input('Insira segundo numero: '))
+def function_Minus(num1, num2):
+    return num1 - num2
 
-resut = function_Plus(num1, num2)
-print(resut)
+def function_Multi(num1, num2):
+    return num1 * num2
+
+def function_Div(num1, num2):
+    return num1 / num2
+
+match decisao:
+    case 1:
+        result = function_Plus(num1, num2)
+        print(result)
+    case 2:
+        result = function_Minus(num1, num2)
+        print(result)
+    case 3:
+        result = function_Multi(num1, num2)
+        print(result)
+    case 4:
+        result = function_Div(num1, num2)
+        print(result)
+
